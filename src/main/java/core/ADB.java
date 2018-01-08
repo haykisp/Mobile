155d1d60f0cc6acf2f10cdd2ada3dd20ec355b85
haykisp <haykisp@gmail.com>
2018-01-08 16:04:13 +0400

    Added Servermanager

--- src/main/java/core/ADB.java
+++ src/main/java/core/ADB.java
@@ -1,5 +1,19 @@
 package core;
 
+import core.managers.ServerManager;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ADB {
+
+    public static String command(String command){
+        if (command.startsWith("adb")) command = command.replace("adb", ServerManager.getAndroidHome()+"/platform-tools/adb");
+        else throw new RuntimeException("This method is designed to run ADB commands only !");
+        String output = ServerManager.runCommand(command);
+        if (output == null) return "";
+        else return output;
+    }
+
 
 }
