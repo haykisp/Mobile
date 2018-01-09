f04b3b60e86e4ce02f9c9e45bcab15d9e636e6ae
haykisp <haykisp@gmail.com>
2018-01-09 14:46:20 +0400

    Added ADB methods

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -26,6 +26,5 @@ public class Runner {
 //        driver.quit();
 
 
-
     }
 }
--- src/main/java/core/Timer.java
+++ src/main/java/core/Timer.java
@@ -6,15 +6,15 @@ public class Timer {
 
     public long startStamp;
 
-    public void start(){
+    public void start() {
         startStamp = getTimeStamp();
     }
 
-    public static long getTimeStamp(){
+    public static long getTimeStamp() {
         return new Date().getTime();
     }
 
-    public boolean expired(int seconds){
+    public boolean expired(int seconds) {
         int difference = (int) ((getTimeStamp() - startStamp) / 1000);
         return difference > seconds;
     }
